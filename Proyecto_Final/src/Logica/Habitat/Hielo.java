071a3590f19f1ab789dbc28dde927ae52e165f01
lulunkaii <sofyiaolate@gmail.com>
2023-11-25 16:49:50 -0300

    Agregada la clase Hielo, heredera de Habitat que implementa sus pokemones permitidos por habitat

--- Proyecto_Final/src/Logica/Habitat/Hielo.java
+++ Proyecto_Final/src/Logica/Habitat/Hielo.java
@@ -1,4 +1,33 @@
 package Logica.Habitat;
 
+import java.util.ArrayList;
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
 public class Hielo extends Habitat {
+    private List<String> pokemonesPermitidos;
+    private Image image;
+
+    public Hielo(){
+        super(new ImageIcon("resources/Ambientes/Hielo.jpg").getImage());
+        this.image = new ImageIcon("resources/Ambientes/Hielo.jpg").getImage();
+        this.pokemonesPermitidos = new ArrayList<>();
+        String[] permitidosHielo = {
+                "Articuno", "Regice", "Snorunt", "Glalie", "Froslass",
+                "Spheal", "Sealeo", "Walrein", "Vanillish", "Vanillite",
+                "Vanilluxe"};
+        for (String pokemon : permitidosHielo){
+            pokemonesPermitidos.add(pokemon);
+        }
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    @Override
+    public boolean permitePokemon(String nombrePokemon) {
+        return pokemonesPermitidos.contains(nombrePokemon);
+    }
 }
