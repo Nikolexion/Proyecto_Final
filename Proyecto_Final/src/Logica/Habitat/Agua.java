030260e54e2bc553ac5d3eb153e656000c87baf0
lulunkaii <sofyiaolate@gmail.com>
2023-11-25 16:46:03 -0300

    Agregada la clase Agua, heredera de Habitat que implementa sus pokemones permitidos por habitat

--- Proyecto_Final/src/Logica/Habitat/Agua.java
+++ Proyecto_Final/src/Logica/Habitat/Agua.java
@@ -1,4 +1,33 @@
 package Logica.Habitat;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.*;
+import java.awt.*;
+
 public class Agua extends Habitat {
+    private List<String> pokemonesPermitidos;
+    private Image image;
+
+    public Agua(){
+        super(new ImageIcon("resources/Ambientes/Agua.jpg").getImage());
+        this.image = new ImageIcon("resources/Ambientes/Agua.jpg").getImage();
+        this.pokemonesPermitidos = new ArrayList<>();
+        String[] permitidosAgua = {
+                "Squirtle","Wartortle", "Blastoise", "Magikarp", "Gyarados",
+                "Lapras", "Omanyte", "Omastar", "Totodile", "Croconaw",
+                "Feraligatr", "Suicune", "Kyogre"};
+        for (String pokemon : permitidosAgua){
+            pokemonesPermitidos.add(pokemon);
+        }
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    @Override
+    public boolean permitePokemon(String nombrePokemon) {
+        return pokemonesPermitidos.contains(nombrePokemon);
+    }
 }
