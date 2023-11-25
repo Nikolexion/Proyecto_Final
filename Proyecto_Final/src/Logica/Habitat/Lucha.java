ce097252ee0c2cfa5de07e93d9505402384b133b
lulunkaii <sofyiaolate@gmail.com>
2023-11-25 16:51:56 -0300

    Agregada la clase Lucha, heredera de Habitat que implementa sus pokemones permitidos por habitat

--- Proyecto_Final/src/Logica/Habitat/Lucha.java
+++ Proyecto_Final/src/Logica/Habitat/Lucha.java
@@ -1,4 +1,32 @@
 package Logica.Habitat;
 
+import java.util.ArrayList;
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
 public class Lucha extends Habitat {
+    private List<String> pokemonesPermitidos;
+    private Image image;
+
+    public Lucha(){
+        super(new ImageIcon("resources/Ambientes/Lucha.jpg").getImage());
+        this.image = new ImageIcon("resources/Ambientes/Lucha.jpg").getImage();
+        this.pokemonesPermitidos = new ArrayList<>();
+        String[] permitidosLucha = {
+                "Hitmonchan", "Hitmonlee", "Machop", "Machamp", "Machoke",
+                "Makuhita", "Hariyama", "Mankey", "Primeape", "Annihilape"};
+        for (String pokemon : permitidosLucha){
+            pokemonesPermitidos.add(pokemon);
+        }
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    @Override
+    public boolean permitePokemon(String nombrePokemon) {
+        return pokemonesPermitidos.contains(nombrePokemon);
+    }
 }
