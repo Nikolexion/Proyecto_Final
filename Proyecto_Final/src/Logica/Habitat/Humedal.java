24d913978f218dc9e4755b311e3ce86f89ea2171
lulunkaii <sofyiaolate@gmail.com>
2023-11-25 16:50:32 -0300

    Agregada la clase Humedal, heredera de Habitat que implementa sus pokemones permitidos por habitat

--- Proyecto_Final/src/Logica/Habitat/Humedal.java
+++ Proyecto_Final/src/Logica/Habitat/Humedal.java
@@ -1,4 +1,33 @@
 package Logica.Habitat;
 
+import java.util.ArrayList;
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
 public class Humedal extends Habitat {
+    private List<String> pokemonesPermitidos;
+    private Image image;
+
+    public Humedal(){
+        super(new ImageIcon("resources/Ambientes/Humedal.jpg").getImage());
+        this.image = new ImageIcon("resources/Ambientes/Humedal.jpg").getImage();
+        this.pokemonesPermitidos = new ArrayList<>();
+        String[] permitidosHumedal = {
+                "Lotad", "Lombre", "Ludicolo", "Mudkip", "Marshtomp",
+                "Swampert", "Psyduck", "Golduck", "Relicanth", "Wooper",
+                "Quagsire"};
+        for (String pokemon : permitidosHumedal){
+            pokemonesPermitidos.add(pokemon);
+        }
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    @Override
+    public boolean permitePokemon(String nombrePokemon) {
+        return pokemonesPermitidos.contains(nombrePokemon);
+    }
 }
