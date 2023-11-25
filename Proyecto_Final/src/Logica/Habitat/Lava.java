2ee70a5a662b488ab6386c1a94721b17914b8bb4
lulunkaii <sofyiaolate@gmail.com>
2023-11-25 16:50:54 -0300

    Agregada la clase Lava, heredera de Habitat que implementa sus pokemones permitidos por habitat

--- Proyecto_Final/src/Logica/Habitat/Lava.java
+++ Proyecto_Final/src/Logica/Habitat/Lava.java
@@ -1,4 +1,33 @@
 package Logica.Habitat;
 
+import java.util.ArrayList;
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
 public class Lava extends Habitat {
+    private List<String> pokemonesPermitidos;
+    private Image image;
+
+    public Lava(){
+        super(new ImageIcon("resources/Ambientes/Lava.jpg").getImage());
+        this.image = new ImageIcon("resources/Ambientes/Lava.jpg").getImage();
+        this.pokemonesPermitidos = new ArrayList<>();
+        String[] permitidosLava = {
+                "Charmander", "Charmeleon", "Charizard", "Cyndaquil", "Quilava",
+                "Typhlosion", "Entei", "Moltres", "Torchic", "Combusken",
+                "Blaziken", "Vulpix", "Ninetales"};
+        for (String pokemon : permitidosLava){
+            pokemonesPermitidos.add(pokemon);
+        }
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    @Override
+    public boolean permitePokemon(String nombrePokemon) {
+        return pokemonesPermitidos.contains(nombrePokemon);
+    }
 }
