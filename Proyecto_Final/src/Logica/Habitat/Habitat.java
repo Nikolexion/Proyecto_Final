5830016d3df92398f1ed70b2e9f512562259ef06
lulunkaii <sofyiaolate@gmail.com>
2023-12-09 19:36:44 -0300

    feat: se agregan funciones de arraylist a habitat: ahora tiene exceptions y se pueden agregar y quitar pokemons

--- Proyecto_Final/src/Logica/Habitat/Habitat.java
+++ Proyecto_Final/src/Logica/Habitat/Habitat.java
@@ -2,21 +2,41 @@ package Logica.Habitat;
 
 import Logica.*;
 import Logica.Comidas.*;
+import Logica.Excepciones.HabitatNoPermitePokemon;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public abstract class Habitat {
     private Image image;
     private Comedero comedero;
     private EnumComidas comidaPreferida;
+    private ArrayList<Pokemon> arrayList;
 
     public Habitat(Image image, EnumComidas comidaPreferida) {
         this.image = image;
         this.comidaPreferida = comidaPreferida;
+        arrayList = new ArrayList<>();
     }
 
     public abstract boolean permitePokemon(Pokemon pokemon);
     public abstract boolean permiteComida(Comida comida);
+    public void addPokemon(Pokemon pokemon) throws HabitatNoPermitePokemon {
+        if (permitePokemon(pokemon)){
+            this.arrayList.add(pokemon);
+        } else {
+            throw new HabitatNoPermitePokemon("El pokemon " + pokemon.getNombre()
+                                              + " no puede ir en este habitat. ");
+        }
+    }
+    public Pokemon getPokemon(Pokemon pokemon){
+        int index = this.arrayList.indexOf(pokemon);
+        if (index != -1) {
+            return this.arrayList.remove(index);
+        } else {
+            return null;
+        }
+    }
 
     public Image getImage() {
         return image;
