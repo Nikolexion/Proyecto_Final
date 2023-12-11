60543d843e8ee99569e0d42d5209531b00425c01
Nikolexion <nfalc2003@gmail.com>
2023-12-10 23:00:29 -0400

    afeat: añadido texto para explicar el uso del pokezoo

--- Proyecto_Final/src/Logica_Grafica/PanelMenuHorz.java
+++ Proyecto_Final/src/Logica_Grafica/PanelMenuHorz.java
@@ -22,6 +22,12 @@ public class PanelMenuHorz extends JPanel {
         g.fillRect(260,623,940,170);
         g.setColor(Color.black);
         g.drawString("Seleccione el habitat que quiere añadir",1210,640);
+        g.drawString("Selecciones un pokemon desde las listas a la izquierda, luego haga click en el habitat",280,640);
+        g.drawString("que desee agregarlo(tenga en cuenta que los pokemon de agua solo pueden ir con los pokemon de agua y asi",
+                280,655);
+        g.drawString("con todos los demas), luego agregue comida haciendo click en las comidas abajo a la izquierda y depues",
+                280,670);
+        g.drawString("click en el habitat que quiere alimentar. Los habitat tienen un maximo de 5 pokemon.",280,685);
 
     }
     public void iniciarLista(){
