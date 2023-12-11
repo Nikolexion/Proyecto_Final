2fe503fb90cef34ec83b6a8b22996f5aa4d5a284
Nikolexion <nfalc2003@gmail.com>
2023-12-10 23:04:09 -0400

    fix: eliminada variable en desuso

--- Proyecto_Final/src/Logica/Inventario.java
+++ Proyecto_Final/src/Logica/Inventario.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
  * */
 public class Inventario {
     private ArrayList<Comida> inventario;
-    private int dinero;
     public Inventario() {
         inventario = new ArrayList<>();
     }
@@ -22,14 +21,5 @@ public class Inventario {
     }
     public Comida getComida(){
         return inventario.remove(0);
-    }
-    public void sumarDinero(int ganancias){
-        this.dinero += ganancias;
-    }
-    public void restarDinero(int gastos){
-        this.dinero -= gastos;
-    }
-    public int getDinero(){
-        return this.dinero;
     }
 }
